5d5f63a269d0530914118fd640d3b9e82c60c081
BerylZhuang <beryl.c.zhuang@gmail.com>
2015-03-04 23:16:34 -0800

    get data script

---
+++ rscripts/get_data.R
@@ -0,0 +1,18 @@
+library(GEOquery)
+library(wateRmelon)
+library(IlluminaHumanMethylation450k.db)
+
+GSE48684 <- getGEO("GSE48684", destdir="./") # get data save to home dir
+show(GSE48684)
+
+
+#get the methylation beta value # only the first 6 rows
+methyl_data <- head(exprs(GSE48684))
+
+#get the metadata
+methyl_metadata <- pData(phenoData(GSE48684))
+
+
+#save Rdata
+save(methyl_data, methyl_metadata, file = "GSE48684.Rdata")
+
