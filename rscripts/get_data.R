9bdfcb9f98ba4ac557ddec8e588edcc8728a88bd
Beryl Zhuang <BerylZhuang@users.noreply.github.com>
2015-03-06 13:38:10 -0800

    Update get_data.R
    The get the full data is commented out, to get the full data, remove the #

--- rscripts/get_data.R
+++ rscripts/get_data.R
@@ -9,6 +9,9 @@ show(GSE48684)
 #get the methylation beta value # only the first 6 rows
 methyl_data <- head(exprs(GSE48684))
 
+#get the full data (commented out)
+#methyl_data <- exprs(GSE48684)
+
 #get the metadata
 methyl_metadata <- pData(phenoData(GSE48684))
 
