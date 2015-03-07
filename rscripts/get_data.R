924da538c1a96907041690b7202d831f0ff5bca5
BerylZhuang <beryl.c.zhuang@gmail.com>
2015-03-06 20:47:13 -0800

    modify the get_data, save the entire methylation data

--- rscripts/get_data.R
+++ rscripts/get_data.R
@@ -1,21 +1,31 @@
-library(GEOquery)
-library(wateRmelon)
-library(IlluminaHumanMethylation450k.db)
+#####################################################
+# Beryl Zhuang
+#
+# Download GSE48684 dataset and save the data
+# and metadata as .Rdata
+#
+# Output: GSE48684_full.Rdata
+#         methyl_data: contain beta value for all probes (no filter)
+#         methyl_metadata: metadata for all samples
+#####################################################
 
-GSE48684 <- getGEO("GSE48684", destdir="../data") # get data save to "data" dir
+library(GEOquery)
+
+GSE48684 <- getGEO("GSE48684", destdir="../data") # get data save to "data" dir # don't commit this data, it's large
 show(GSE48684)
 
 
-#get the methylation beta value # only the first 6 rows
-methyl_data <- head(exprs(GSE48684))
-
-#get the full data (commented out)
-#methyl_data <- exprs(GSE48684)
+#get the methylation beta value for the full data
+methyl_data <- exprs(GSE48684)
 
 #get the metadata
 methyl_metadata <- pData(phenoData(GSE48684))
 
 
 #save Rdata
-save(methyl_data, methyl_metadata, file = "../data/GSE48684_head.Rdata")
+save(methyl_data, methyl_metadata, file = "../data/GSE48684_full.Rdata")
 
+
+#####################################################
+# End of script
+#####################################################
