808a4d999758075e53a7b7a257c1beb9cf0b2920
Ka Ming Nip <kamingnip@gmail.com>
2015-04-04 12:27:31 -0700

    fixed, one left to debug!

--- rscripts/03limma.R
+++ rscripts/03limma.R
@@ -26,11 +26,13 @@ limmaTopTable <- function(dat, des){
 
 # design matrix for limma on group
 des <- metadata %>% 
-  select(group, geo_accession, colon_region, gender, stage)
+  select(group, colon_region, gender, stage)
+rownames(des) <- metadata$geo_accession
+
+
 desMat.group <- model.matrix(~group, des)
 
 # remove NA from data
-
 nums <- sapply(M.norm.CGI, is.numeric)
 dat <- na.omit(M.norm.CGI[ , nums])
 
@@ -39,39 +41,34 @@ topTable.group <- limmaTopTable(dat, desMat.group)
 
 # design matrix for group and gender
 des.group.gender <- des %>%
-  select(group, geo_accession, gender)
+  select(group, gender)
 desMat.group.gender <- model.matrix(~group+gender, des.group.gender)
 
-dat.group.gender <- dat[, as.character(des.group.gender$geo_accession)]
+dat.group.gender <- dat[, rownames(des.group.gender)]
 
 topTable.group.gender <- limmaTopTable(dat.group.gender, desMat.group.gender)
 
 
-# WORKS UP TO HERE
-
-
 # design matrix for cancer and stage
-des.cancer.stage <- subset(des, group == "cancer")
+des.cancer.stage <- droplevels(subset(des, group == "cancer"))
 desMat.cancer.stage <- model.matrix(~stage, des.cancer.stage)
 
-dat.cancer.stage <- dat[, as.character(des.cancer.stage$geo_accession)]
+dat.cancer.stage <- dat[, rownames(des.cancer.stage)]
 
-myFit <- lmFit(dat.cancer.stage, desMat.cancer.stage)
-myEbFit <- eBayes(myFit)
-stop()
-
-myTopTable <- topTable(myEbFit, number=nrow(dat), colnames(coef(myEbFit)))
+#myFit <- lmFit(dat.cancer.stage, desMat.cancer.stage)
+#myEbFit <- eBayes(myFit)
+#myTopTable <- topTable(myEbFit, number=nrow(dat), colnames(coef(myEbFit)))
 
 topTable.cancer.stage <- limmaTopTable(dat.cancer.stage, desMat.cancer.stage)
 
-
+# WORKS UP TO HERE
 
 # design matrix for limma on group and colon_region
-des.no.unknown.region <- subset(des, colon_region != 'unknown')
+des.no.unknown.region <- droplevels(subset(des, colon_region != 'unknown'))
 desMat.group.plus.region <- model.matrix(~group+colon_region, des.no.unknown.region)
 desMat.group.star.region <- model.matrix(~group*colon_region, des.no.unknown.region)
 
-dat.no.unknown.region <- dat[, as.character(des.no.unknown.region$geo_accession)]
+dat.no.unknown.region <- dat[, rownames(des.no.unknown.region)]
 
 topTable.group.plus.region <- limmaTopTable(dat.no.unknown.region, desMat.group.plus.region)
 topTable.group.star.region <- limmaTopTable(dat.no.unknown.region, desMat.group.star.region)
