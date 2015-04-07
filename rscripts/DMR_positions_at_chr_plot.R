d6ecec6bd0fcbb32f5b989bf84fa2b661db19539
Rashedul Islam <rashedul.gen@gmail.com>
2015-04-06 21:18:18 -0700

    wip

--- rscripts/DMR_positions_at_chr_plot.R
+++ rscripts/DMR_positions_at_chr_plot.R
@@ -1,10 +1,9 @@
 #####################################################
 # Rashedul Islam
 #
-#
-# load the topTable data "../data/topTable.*.RData" 
+# Input: DMRs obtained from four different DMA experiments
 # 
-# save the plots
+# Output: DMR plots
 # 
 #####################################################
 library(ggplot2)
@@ -16,16 +15,18 @@ load("../data//chrlen.RData")
 load("../data//coord.RData")
 
 
-#subset data
+#subset test data 
 #DMR1.1  <- subset(adenoma_cancer_dma, adenoma_cancer_dma$P.Value < 1e-3)
 #str(DMR1.1)
 
-#####test data end
-
 p1  <- read.table("../data//limma/adenoma_vs_cancer_santina.tsv")
+p1_subset  <- subset(p1, p1$q.value < 1e-4)
 p2  <- read.table("../data//limma/normalC_vs_normalH_santina.tsv")
+p2_subset  <- subset(p2, p2$q.value < 1e-4)
 p3  <- read.table("../data//limma/normal_vs_adenoma_santina.tsv")
+p3_subset  <- subset(p3, p3$q.value < 1e-4)
 p4  <- read.table("../data//limma/normal_vs_cancer_santina.tsv")
+p4_subset  <- subset(p4, p4$q.value < 1e-4)
 
 drawplot <- function(DMR){
   # coordinates of probes in DM CGIs
@@ -47,24 +48,21 @@ drawplot <- function(DMR){
   return(coord.plot)
 }
 
-#str(p4)
-#plot1  <- drawplot("../data//limma/")
-#plot
 # now plot coordinates of MDR for probes
-Plot_adenoma_vs_cancer  <- drawplot(p1)
+Plot_adenoma_vs_cancer  <- drawplot(p1_subset)
 Plot_adenoma_vs_cancer
-Plot_normalC_vs_normalH <- drawplot(p2)
+Plot_normalC_vs_normalH <- drawplot(p2_subset)
 Plot_normalC_vs_normalH
-Plot_normal_vs_adenoma  <- drawplot(p3)
+Plot_normal_vs_adenoma  <- drawplot(p3_subset)
 Plot_normal_vs_adenoma
-Plot_normal_vs_cancer <- drawplot(p4)
+Plot_normal_vs_cancer <- drawplot(p4_subset)
 Plot_normal_vs_cancer
 
-#ggsave(plot = coord.plot, filename = "../figures/coord.plot.png", width = 16, height = 10.67, units = "in")
-ggsave(plot = Plot_adenoma_vs_cancer, filename = "../figures/Plot_adenoma_vs_cancer.png", width = 16, height = 10.67, units = "in")
-ggsave(plot = Plot_normalC_vs_normalH, filename = "../figures/Plot_normalC_vs_normalH.png", width = 16, height = 10.67, units = "in")
-ggsave(plot = Plot_normal_vs_adenoma, filename = "../figures/Plot_normal_vs_adenoma.png", width = 16, height = 10.67, units = "in")
-ggsave(plot = Plot_normal_vs_cancer, filename = "../figures/Plot_normal_vs_cancer.png", width = 16, height = 10.67, units = "in")
+
+ggsave(plot = Plot_adenoma_vs_cancer, filename = "../analysis_reports/06positions_at_DMR/Plot_adenoma_vs_cancer.png", width = 16, height = 10.67, units = "in")
+ggsave(plot = Plot_normalC_vs_normalH, filename = "../analysis_reports/06positions_at_DMR/Plot_normalC_vs_normalH.png", width = 16, height = 10.67, units = "in")
+ggsave(plot = Plot_normal_vs_adenoma, filename = "../analysis_reports/06positions_at_DMR/Plot_normal_vs_adenoma.png", width = 16, height = 10.67, units = "in")
+ggsave(plot = Plot_normal_vs_cancer, filename = "../analysis_reports/06positions_at_DMR/Plot_normal_vs_cancer.png", width = 16, height = 10.67, units = "in")
 
 #####################################################
 # End of script
